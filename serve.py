56d7791a57975e4f29e337bf284e30e2ab0e4807
rad-haha <shenasieun@gmail.com>
2025-09-14 17:49:47 +0900

    Update serve.py
    아아아악!!!!!!!1

--- serve.py
+++ serve.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from crawler_radhaha.beready_crawler_core import router as lilacdiet_router
-from main_yolo2 import router as yolo_router
+from main_yolo2 import router as yolo_router, init_db
 
 app = FastAPI(title="Beready API (YOLO + LilacDiet)")
 
+# 시작할때마다 초기화 및 db 생성 보장
+@app.on_event("startup")
+def startup_event():
+    init_db
+    
 # 라우터 합치기: 두 모듈을 하나로 합쳐 서버에 띄우기
 app.include_router(lilacdiet_router)
 app.include_router(yolo_router)
